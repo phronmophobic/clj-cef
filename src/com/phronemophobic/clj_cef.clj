1ffdd4874c3982c5539af6f27cd2b36481227b43
Adrian Smith <smith.adriane@gmail.com>
2021-02-01 20:37:42 -0800

    initial commit.

---
+++ src/com/phronemophobic/clj_cef.clj
@@ -0,0 +1,7 @@
+(ns com.phronemophobic.clj-cef
+  (:require [net.n01se.clojure-jna :as jna]))
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (prn x "Hello, World!"))
---
+++ test/com/phronemophobic/clj_cef_test.clj
@@ -0,0 +1,7 @@
+(ns com.phronemophobic.clj-cef-test
+  (:require [clojure.test :refer :all]
+            [com.phronemophobic.clj-cef :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
