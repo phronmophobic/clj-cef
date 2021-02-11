910a950938440791809cd79dbcacbd4809eeac89
Adrian Smith <smith.adriane@gmail.com>
2021-02-10 20:55:53 -0800

    compile java classes with target 1.8.

--- javac.sh
+++ javac.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-find javasrc -name '*.java' -print0 | xargs -0 javac -d classes -cp `clj -Spath` 
+find javasrc -name '*.java' -print0 | xargs -0 javac -target 1.8 -source 1.8 -d classes -cp `clj -Spath`
