81d15f363fee4b13f3dc9b6ec2ab396b14f5b070
Adrian Smith <smith.adriane@gmail.com>
2021-03-06 13:41:43 -0800

    make cef dir configurable in compile macosx script.

--- csource/compile_macosx.sh
+++ csource/compile_macosx.sh
@@ -7,10 +7,11 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd "$DIR"
 
 export SDKROOT="/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk"
+CEF_DIR="/Volumes/My Passport for Mac/backup/cef/cef_binary_88.1.6+g4fe33a1+chromium-88.0.4324.96_macosx64"
 
 # test
 clang++ \
-    -I '/Volumes/My Passport for Mac/backup/cef/cef_binary_88.1.6+g4fe33a1+chromium-88.0.4324.96_macosx64' \
+    -I "$CEF_DIR" \
     -framework OpenGL \
     -framework Cocoa \
     -framework IOKit \
@@ -21,7 +22,7 @@ clang++ \
     -framework CoreServices \
     -framework Foundation \
     -mmacosx-version-min=10.11 \
-    '/Volumes/My Passport for Mac/backup/cef/cef_binary_88.1.6+g4fe33a1+chromium-88.0.4324.96_macosx64/build/libcef_dll_wrapper/Debug/libcef_dll_wrapper.a' \
+    "$CEF_DIR/build/libcef_dll_wrapper/Debug/libcef_dll_wrapper.a" \
     -std=c++17 \
     -DTEST \
     -o ceftest \
@@ -29,7 +30,7 @@ clang++ \
     cefclj.cpp
 
 clang++ \
-    -I '/Volumes/My Passport for Mac/backup/cef/cef_binary_88.1.6+g4fe33a1+chromium-88.0.4324.96_macosx64' \
+    -I "$CEF_DIR" \
     -framework OpenGL \
     -framework Cocoa \
     -framework IOKit \
@@ -40,11 +41,11 @@ clang++ \
     -framework CoreServices \
     -framework Foundation \
     -mmacosx-version-min=10.11 \
-    '/Volumes/My Passport for Mac/backup/cef/cef_binary_88.1.6+g4fe33a1+chromium-88.0.4324.96_macosx64/build/libcef_dll_wrapper/Debug/libcef_dll_wrapper.a' \
-    '/Volumes/My Passport for Mac/backup/cef/cef_binary_88.1.6+g4fe33a1+chromium-88.0.4324.96_macosx64/Debug/cef_sandbox.a' \
+    "$CEF_DIR/build/libcef_dll_wrapper/Debug/libcef_dll_wrapper.a" \
+    "$CEF_DIR/Debug/cef_sandbox.a" \
     -std=c++17 \
     -DHELPER \
-    -o 'ceflib Helper' \
+    -o "ceflib Helper" \
     thirdparty/backupsignalhandlers/signal_restore_posix.cpp \
     getdir.mm \
     cefclj.cpp
@@ -53,7 +54,7 @@ clang++ \
 #dylib
 
 clang++ \
-    -I '/Volumes/My Passport for Mac/backup/cef/cef_binary_88.1.6+g4fe33a1+chromium-88.0.4324.96_macosx64' \
+    -I "$CEF_DIR" \
     -framework OpenGL \
     -framework Cocoa \
     -framework IOKit \
@@ -64,7 +65,7 @@ clang++ \
     -framework CoreServices \
     -framework Foundation \
     -mmacosx-version-min=10.11 \
-    '/Volumes/My Passport for Mac/backup/cef/cef_binary_88.1.6+g4fe33a1+chromium-88.0.4324.96_macosx64/build/libcef_dll_wrapper/Debug/libcef_dll_wrapper.a' \
+    "$CEF_DIR/build/libcef_dll_wrapper/Debug/libcef_dll_wrapper.a" \
     -std=c++17 \
     -dynamiclib \
     -o libcljcef.dylib \
