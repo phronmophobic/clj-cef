ad44ad27f3ed7516b96136b86b5b16395ffa8ae9
Adrian Smith <smith.adriane@gmail.com>
2022-10-16 15:44:53 -0700

    parameterize compile script.

--- csource/compile_macosx.sh
+++ csource/compile_macosx.sh
@@ -7,9 +7,43 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd "$DIR"
 
 export SDKROOT="/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk"
-CEF_DIR="/Volumes/My Passport for Mac/backup/cef/cef_binary_88.1.6+g4fe33a1+chromium-88.0.4324.96_macosx64"
+# CEF_DIR="/Volumes/My Passport for Mac/backup/cef/cef_binary_88.1.6+g4fe33a1+chromium-88.0.4324.96_macosx64"
+# CEF_DIR="/Users/adrian/Downloads/cef_binary_88.1.6+g4fe33a1+chromium-88.0.4324.96_macosarm64_minimal"
+ARCH="$1"
+PLATFORM="macos"
+CEF_DIR="$2"
+CEF_DLL="$CEF_DIR/libcef_dll_wrapper/libcef_dll_wrapper.a"
 
-# test
+# # # test
+# clang++ \
+#     -I "$CEF_DIR" \
+#     -framework OpenGL \
+#     -framework Cocoa \
+#     -framework IOKit \
+#     -framework CoreFoundation \
+#     -framework CoreVideo \
+#     -framework AppKit \
+#     -framework CoreGraphics \
+#     -framework CoreServices \
+#     -framework Foundation \
+#     -mmacosx-version-min=10.11 \
+#     "$CEF_DLL" \
+#     -std=c++17 \
+#     -DTEST \
+#     -o ceftest \
+#     thirdparty/backupsignalhandlers/signal_restore_posix.cpp \
+#     cefclj.cpp
+
+
+# ./compile_macosx.sh arm64 /tmp/com.phronemophobic.cef/cef_binary_88.1.6+g4fe33a1+chromium-88.0.4324.96_macosarm64_minimal/
+
+pushd "$CEF_DIR"
+
+cmake -DCMAKE_BUILD_TYPE=Release -G "Unix Makefiles" -DPROJECT_ARCH="$ARCH" .
+make
+
+popd
+
 clang++ \
     -I "$CEF_DIR" \
     -framework OpenGL \
@@ -22,30 +56,11 @@ clang++ \
     -framework CoreServices \
     -framework Foundation \
     -mmacosx-version-min=10.11 \
-    "$CEF_DIR/build/libcef_dll_wrapper/Debug/libcef_dll_wrapper.a" \
-    -std=c++17 \
-    -DTEST \
-    -o ceftest \
-    thirdparty/backupsignalhandlers/signal_restore_posix.cpp \
-    cefclj.cpp
-
-clang++ \
-    -I "$CEF_DIR" \
-    -framework OpenGL \
-    -framework Cocoa \
-    -framework IOKit \
-    -framework CoreFoundation \
-    -framework CoreVideo \
-    -framework AppKit \
-    -framework CoreGraphics \
-    -framework CoreServices \
-    -framework Foundation \
-    -mmacosx-version-min=10.11 \
-    "$CEF_DIR/build/libcef_dll_wrapper/Debug/libcef_dll_wrapper.a" \
-    "$CEF_DIR/Debug/cef_sandbox.a" \
+    "$CEF_DLL" \
+    "$CEF_DIR/Release/cef_sandbox.a" \
     -std=c++17 \
     -DHELPER \
-    -o "ceflib Helper" \
+    -o "ceflib Helper"-"$PLATFORM"-"$ARCH" \
     thirdparty/backupsignalhandlers/signal_restore_posix.cpp \
     getdir.mm \
     cefclj.cpp
@@ -65,16 +80,16 @@ clang++ \
     -framework CoreServices \
     -framework Foundation \
     -mmacosx-version-min=10.11 \
-    "$CEF_DIR/build/libcef_dll_wrapper/Debug/libcef_dll_wrapper.a" \
+    "$CEF_DLL" \
     -std=c++17 \
     -dynamiclib \
-    -o libcljcef.dylib \
+    -o libcljcef-"$PLATFORM"-"$ARCH".dylib \
     thirdparty/backupsignalhandlers/signal_restore_posix.cpp \
     getdir.mm \
     cefclj.cpp
 
 
-cp libcljcef.dylib ../resources/darwin
-cp 'ceflib Helper' ../resources/darwin
+# cp libcljcef.dylib ../resources/darwin
+# cp 'ceflib Helper' ../resources/darwin
 
 echo 'done'
